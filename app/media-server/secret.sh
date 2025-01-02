24f9c468364297a95d4c3a5c8c1dd2db4f797e43
Adam Roland <roland.adam@hotmail.com>
2025-01-02 09:54:29 +0100

    remove files

--- app/media-server/secret.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl create secret generic k3s-media-server -n media-server \
-  --from-literal=LETS_ENCRYPT_EMAIL=roland.adam@hotmail.com \
-  --from-literal=TRANSMISSION_PASSWORD= \
-  --from-literal=DOMAIN=home.adaminformatika.hu
-
-
